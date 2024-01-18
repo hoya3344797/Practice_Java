33de2621971ee716db535cc2772e4cdfd27a8202
O._.Hou <hoya3344797@gmail.com>
2024-01-18 20:37:40 +0900

    폴더 추가

---
+++ JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //문자형
+        System.out.println("안녕하세요?"); //문자형
+        System.out.println(12); //숫자 자료형
+        System.out.println(-34); //숫자 자료형
+        System.out.println(3.14); //숫자 자료형
+        System.out.println(true); //불리안 자료형
+        System.out.println(false); //불리안 자료형
+
+        System.out.println(123 + 345); //연산 자료형
+
+    }
+}
